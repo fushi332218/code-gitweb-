f69ecd6966e1a9fe64f3f4a8905868c752ace24f
fushi332218 <2087495494@qq.com>
2023-05-24 17:48:56 +0800

    “拳皇项目“making

--- “拳皇”项目/static/js/base.js
+++ “拳皇”项目/static/js/base.js
@@ -1,9 +1,29 @@
+import { GameMap } from '/“拳皇”项目/static/js/game_map/base.js';
+import { Kyo } from '/“拳皇”项目/static/js/player/kyo.js'
+
 class KOF {
     constructor(id) {
         this.$kof = $('#' + id);
 
-        console.log(this.$kof);
-
+        this.game_map = new GameMap(this);
+        this.players = [
+            new Kyo(this, {
+                id: 0,
+                x: 200,
+                y: 0,
+                width: 150,
+                height: 200,
+                color: 'blue',
+            }),
+            new Kyo(this, {
+                id: 1,
+                x: 900,
+                y: 0,
+                width: 150,
+                height: 200,
+                color: 'red',
+            }),
+        ];
     }
 }
 
---
+++ “拳皇”项目/static/js/game_map/base.js
@@ -0,0 +1,30 @@
+import { GameObject } from '/“拳皇”项目/static/js/game_object/base.js'
+import { Controller } from '/“拳皇”项目/static/js/controller/base.js'
+
+export class GameMap extends GameObject {
+    constructor(root) {
+        super();
+
+        this.root = root;
+        this.$canvas = $('<canvas width="1280" height="720" tabindex=0></canvas>');
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.root.$kof.append(this.$canvas);
+        this.$canvas.focus();
+
+        this.controller = new Controller(this.$canvas)
+    }
+
+    start() {
+
+    }
+
+    update() {
+        this.render();
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        // this.ctx.fillStyle = 'black';
+        // this.ctx.fillRect(0, 0, this.$canvas.width(), this.$canvas.height());
+    }
+}
